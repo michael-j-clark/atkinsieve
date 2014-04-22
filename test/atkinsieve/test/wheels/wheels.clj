1899bc538f17b982cc04c583b34523f716387e19
michael-j-clark <github@abstractproduct.com>
2014-04-22 12:33:44 -0500

    developing first wheel

--- test/atkinsieve/test/wheels/wheels.clj
+++ test/atkinsieve/test/wheels/wheels.clj
@@ -1,18 +1,21 @@
 (ns
   atkinsieve.test.wheels.wheels
   (:use clojure.test )
-  (:use atkinsieve.wheels.wheels)
-  )
-(def w1  (->WheelOne 20))
+  (:use atkinsieve.wheels.wheels))
+(def lim 200000)
+(def w1  (->WheelOne lim))
 (deftest test-test (is (= 1 1)))
 
-(deftest wheel-one-test
-  
-  (is (= 80 (calculate w1 2 8))))
 
 (deftest wheel-one-calculate-test
-  (let [v1 [143 2 3]] 
-  (is (= (map #(+ (* 4 (* %1 %1)) (* %2 %2)) v1 [4 5988 6]) 
-         (map #(calculate w1 %1 %2) v1 [4 5988 6])))))
+  (let [v1 [143 2 3] v2 [4 5988 6]] 
+    (is (= (map #(+ (* 4 (* %1 %1)) (* %2 %2)) v1 v2) 
+           (map #(calculate w1 %1 %2) v1 v2)))))
+		 
+(deftest wheel-one-tst-test
+  (let [v1 [143 200 3 234 542235 45 12 86 77]] 
+    (is (= (map #(and  (<= % lim) 
+	               (or (= (mod %1 12) 1) (= (mod %1 12) 5))) v1) 
+           (map #(tst w1 %1 ) v1)))))
 
 (run-all-tests)
