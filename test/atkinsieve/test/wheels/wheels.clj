16a058b8f42edce55782e96017af0af796395ab6
michael-j-clark <github@abstractproduct.com>
2014-04-22 10:33:20 -0500

    improved wheels.clj test

--- test/atkinsieve/test/wheels/wheels.clj
+++ test/atkinsieve/test/wheels/wheels.clj
@@ -3,11 +3,16 @@
   (:use clojure.test )
   (:use atkinsieve.wheels.wheels)
   )
-
+(def w1  (->WheelOne 20))
 (deftest test-test (is (= 1 1)))
 
 (deftest wheel-one-test
-  (def w1  (->WheelOne 20))
+  
   (is (= 80 (calculate w1 2 8))))
+
+(deftest wheel-one-calculate-test
+  (let [v1 [143 2 3]] 
+  (is (= (map #(+ (* 4 (* %1 %1)) (* %2 %2)) v1 [4 5988 6]) 
+         (map #(calculate w1 %1 %2) v1 [4 5988 6])))))
 
 (run-all-tests)
