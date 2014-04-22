2362f7c0c523321d58a358369d650ddf7e580570
michael-j-clark <github@abstractproduct.com>
2014-04-22 12:31:45 -0500

    developing first wheel

--- src/atkinsieve/wheels/wheels.clj
+++ src/atkinsieve/wheels/wheels.clj
@@ -1,43 +1,27 @@
 (ns
-  ^{:author michaelclark}
   atkinsieve.wheels.wheels
-  (:require [clojure.core.match :refer [match]]))
- ;TODO: require clojure.test
-(use '[clojure.test :only [is]])
-
-
-(defn square  [x]   (* x x))
-(defn times   [x y] (* x y))
-(defn plus    [x y] (+ x y))
+  (:require [clojure.core.match :refer [match]])
+  (:use [atkinsieve.math.mathfns]))
 
 
 
-(def ^:dynamic lim)
 (defprotocol WheelFact
   (calculate [this x y])
-  (test [this n]))
+  (tst [this n]))
 
 
 
 (defrecord WheelOne
   [lim]
   WheelFact
-  (calculate [this x y]  (plus (times 4 (square x)) (square y) ))
-  (test
+  (calculate [this x y]  (plus (multiply 4 (square x)) (square y) ))
+  (tst
     [this n]
-    (is (true? (and (<= n lim) (or (== (mod n 12) 1)  (= (mod n 12) 5)))))))
-
-
-; time to write some tests
-
-;(def a (WheelOne. 20))
-
-;(calculate a 2 7)
-
-
-
-
-
-
+    (true? (and (<= n lim) (or (n-mod-x-eq n 12 1)  (n-mod-x-eq n 12 5))))))
+	
+	
+(defn -main []
+  (let  [lim 200000 w1  (->WheelOne lim)]
+    (prn (map #(tst w1 %1 ) [23 54 75]))))
 
 
