4cdb339613054aeace1ba7453ccd566a63cf98bf
michael-j-clark <michaelclark@abstractproduct.com>
2014-04-22 21:05:49 -0500

    tests for "Two"

--- test/atkinsieve/test/wheels/wheels.clj
+++ test/atkinsieve/test/wheels/wheels.clj
@@ -4,6 +4,8 @@
   (:use atkinsieve.wheels.wheels))
 (def lim 200000)
 (def w1  (->WheelOne lim))
+(def w2  (->WheelTwo lim))
+
 (deftest test-test (is (= 1 1)))
 
 
@@ -17,5 +19,17 @@
     (is (= (map #(and  (<= % lim) 
 	               (or (= (mod %1 12) 1) (= (mod %1 12) 5))) v1) 
            (map #(tst w1 %1 ) v1)))))
+
+
+(deftest wheel-two-calculate-test
+  (let [v1 [143 2 3] v2 [4 5988 6]]
+    (is (= (map #(+ (* 3 (* %1 %1)) (* %2 %2) ) v1 v2)
+          (map #(calculate w2 %1 %2) v1 v2)))))
+
+(deftest wheel-two-tst-test
+  (let [v1 [143 200 3 234 542235 45 12 86 77]]
+    (is (= (map #(and  (<= % lim)
+                    (= (mod %1 12) 7)) v1)
+          (map #(tst w2 %1 ) v1)))))
 
 (run-all-tests)
