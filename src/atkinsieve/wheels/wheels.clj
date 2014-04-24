0df7b0fe36da08ebd81fcc9ba003a878ab1ca942
michael-j-clark <github@abstractproduct.com>
2014-04-23 19:21:23 -0500

    wheels two and three

--- src/atkinsieve/wheels/wheels.clj
+++ src/atkinsieve/wheels/wheels.clj
@@ -18,10 +18,27 @@
   (tst
     [this n]
     (true? (and (<= n lim) (or (n-mod-x-eq n 12 1)  (n-mod-x-eq n 12 5))))))
+
+
+
+(defrecord WheelTwo
+  [lim]
+  WheelFact
+  (calculate [this x y]  (plus (multiply 3 (square x)) (square y) ))
+  (tst
+    [this n]
+    (true? (and (<= n lim) (n-mod-x-eq n 12 7)))))
+
+
+(defrecord WheelThree
+  [lim]
+  WheelFact
+  (calculate [this x y]  (minus (multiply 3 (square x)) (square y) ))
+  (tst
+    [this n]
+    (true? (and (<= n lim) (n-mod-x-eq n 12 11) ))))
+
+;todo (x>y) condition in wheel three test?
 	
-	
-(defn -main []
-  (let  [lim 200000 w1  (->WheelOne lim)]
-    (prn (map #(tst w1 %1 ) [23 54 75]))))
 
 
