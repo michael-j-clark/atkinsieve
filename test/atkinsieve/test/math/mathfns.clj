19688d0455d4913a5bc88ee5bb9d1da6000d6bd1
michael-j-clark <github@abstractproduct.com>
2014-04-23 19:49:07 -0500

    added test for (minus _ _)

--- test/atkinsieve/test/math/mathfns.clj
+++ test/atkinsieve/test/math/mathfns.clj
@@ -1,9 +1,7 @@
 (ns
   atkinsieve.test.math.mathfns
   (:use clojure.test )
-  (:use atkinsieve.math.mathfns)
-
-  )
+  (:use atkinsieve.math.mathfns))
 
 (deftest squares-test
   (let [r 123]
@@ -11,10 +9,14 @@
 
 (deftest multiply-test
   (let [q 32289 r 5]
-    (is (= (map #(multiply % q) (range r)) (map #(* % q) (range r))))) )
+    (is (= (map #(multiply % q) (range r)) (map #(* % q) (range r))))))
+
+(deftest minus-test
+  (let [q 32289 r 5]
+    (is (= (map #(minus % q) (range r)) (map #(- % q) (range r))))))
 
 (deftest plus-test
   (let [q 1763129 r 5]
-    (is (= (map #(plus % q) (range r)) (map #(+ % q) (range r))))) )
+    (is (= (map #(plus % q) (range r)) (map #(+ % q) (range r))))))
 
 (run-all-tests)
