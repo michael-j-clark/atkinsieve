41f9597c93e7a16821e42299f0771ae76bd726d0
michael-j-clark <michaelclark@abstractproduct.com>
2014-04-27 21:30:54 -0500

    working on collection

--- src/atkinsieve/sieve/sieve.clj
+++ src/atkinsieve/sieve/sieve.clj
@@ -1,2 +1,10 @@
 (ns
   atkinsieve.sieve.sieve)
+
+
+(defn sieve-of-atkin
+  "optimized Sieve of Eratosthenes"
+  [lim]
+  (binding [coll (apply vector (cons 1 (cons 1 (take (- lim 2) (repeat false)))))]
+
+  ))
