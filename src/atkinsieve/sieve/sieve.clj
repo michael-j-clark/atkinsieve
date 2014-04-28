24e29c579cbd35156011c07e4d82d67fc728fd30
michael-j-clark <michaelclark@abstractproduct.com>
2014-04-27 22:36:15 -0500

    just calling sieve fn w/ example how to get and apply limit argument

--- src/atkinsieve/sieve/sieve.clj
+++ src/atkinsieve/sieve/sieve.clj
@@ -1,10 +1,17 @@
 (ns
   atkinsieve.sieve.sieve)
 
-
+(def ^:dynamic coll)
 (defn sieve-of-atkin
   "optimized Sieve of Eratosthenes"
   [lim]
-  (binding [coll (apply vector (cons 1 (cons 1 (take (- lim 2) (repeat false)))))]
-
+  (binding [coll (apply vector (cons 1 (cons 1(take (- lim 2) (repeat false)))))]
+    (prn (assoc  coll 3 true))
   ))
+
+
+(defn -main "main function"
+  [& args]
+  (sieve-of-atkin (read-string (first args)) )
+
+)
