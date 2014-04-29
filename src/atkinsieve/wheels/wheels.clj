a800a2810684d6412344ad481986bddf920ba8fa
michael-j-clark <github@abstractproduct.com>
2014-04-28 22:03:46 -0500

    working wood wheels

--- src/atkinsieve/wheels/wheels.clj
+++ src/atkinsieve/wheels/wheels.clj
@@ -6,33 +6,60 @@
 
 (defprotocol WheelFact
   (calculate [this x y])
-  (tst [this n]))
+  (tst [this n])
+  (factor [this n x y])
+  )
 
 (defrecord WheelOne
-  [lim]
+  [lim coll]
   WheelFact
   (calculate [this x y]  (plus (multiply 4 (square x)) (square y) ))
   (tst
     [this n]
-    (true? (and (<= n lim) (or (n-mod-x-eq n 12 1)  (n-mod-x-eq n 12 5))))))
+    (true? (and (<= n lim) (or (n-mod-x-eq n 12 1)  (n-mod-x-eq n 12 5)))))
+  (factor [this n x y]
+    (let [ n1 (calculate this x y) ]
+      (if (tst this n1))
+        (factor (->WheelTwo lim  (flipwheel n1 coll) x y))
+        (factor (->WheelTwo lim  coll) x y))))
 
 
 (defrecord WheelTwo
-  [lim]
+  [lim coll]
   WheelFact
   (calculate [this x y]  (plus (multiply 3 (square x)) (square y) ))
   (tst
     [this n]
-    (true? (and (<= n lim) (n-mod-x-eq n 12 7)))))
+    (true? (and (<= n lim) (n-mod-x-eq n 12 7))))
+  (factor [this n x y]
+    (let [ n2 (calculate this x y) ]
+      (if (tst this n2))
+        (factor (->WheelThree lim  (flipwheel n2 coll) x y))
+        (factor (->WheelThree lim  coll) x y))))
+
 
 
 (defrecord WheelThree
-  [lim]
+  [lim coll]
   WheelFact
   (calculate [this x y]  (cond (> x y) (minus (multiply 3 (square x)) (square y) ) :else (inc lim)))
   (tst
     [this n]
-    (true? (and (<= n lim) (n-mod-x-eq n 12 11) ))))
+    (true? (and (<= n lim) (n-mod-x-eq n 12 11) )))
+  (factor [this n x y]
+    (let [ n3 (calculate this x y) ]
+      (if (tst this n3))
+        (flipwheel n3 coll)
+        coll)))
+
+
+(defn- flipwheel
+  "toggle boolean value"
+  [n coll]
+  (if (get coll (dec n))
+    (assoc coll (int (dec n)) false)
+    (assoc coll (int (dec n)) true)))
+
 
 
 	
