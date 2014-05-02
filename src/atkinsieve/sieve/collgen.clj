72f9f0ec7a11cc3e3b7bdc9f5ea44782437d270e
michael-j-clark <github@abstractproduct.com>
2014-05-01 23:22:38 -0500

    Change name of function to (gen-coll a:int)

--- src/atkinsieve/sieve/collgen.clj
+++ src/atkinsieve/sieve/collgen.clj
@@ -2,5 +2,5 @@
   atkinsieve.sieve.collgen)
 
  ;Had this is sieve.sieve but there was cyclic dependency error bec. is needed in wheels.wheels
-(defn get-coll "make the collection, a boolean vector w/ first two elems true" [lim]
+(defn gen-coll "make the collection, a boolean vector w/ first two elems true" [lim]
   (apply vector (cons 1 (cons 1(take (- lim 2) (repeat false))))))
