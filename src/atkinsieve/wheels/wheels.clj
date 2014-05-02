f29fb3fa0e7b39c964430aa7b4471f995447003c
michael-j-clark <github@abstractproduct.com>
2014-05-01 23:10:42 -0500

    Debugging additions
    * Some parentheses were misplaced
    
    * Got unit tests to run

--- src/atkinsieve/wheels/wheels.clj
+++ src/atkinsieve/wheels/wheels.clj
@@ -1,27 +1,35 @@
 (ns
   atkinsieve.wheels.wheels
   (:require [clojure.core.match :refer [match]])
-  (:use [atkinsieve.math.mathfns]))
+  (:use [atkinsieve.math.mathfns])
+  (:use atkinsieve.sieve.collgen))
 
 
 (defprotocol WheelFact
   (calculate [this x y])
   (tst [this n])
-  (factor [this n x y])
+  (factor [this x y])
   )
 
-(defrecord WheelOne
+(defn- flipwheel
+  "toggle boolean value"
+  [n coll]
+  (if (get coll (dec n))
+    (assoc coll (int (dec n)) false)
+    (assoc coll (int (dec n)) true)))
+
+(defrecord WheelThree
   [lim coll]
   WheelFact
-  (calculate [this x y]  (plus (multiply 4 (square x)) (square y) ))
+  (calculate [this x y]  (cond (> x y) (minus (multiply 3 (square x)) (square y) ) :else (inc lim)))
   (tst
     [this n]
-    (true? (and (<= n lim) (or (n-mod-x-eq n 12 1)  (n-mod-x-eq n 12 5)))))
-  (factor [this n x y]
-    (let [ n1 (calculate this x y) ]
-      (if (tst this n1))
-        (factor (->WheelTwo lim  (flipwheel n1 coll) x y))
-        (factor (->WheelTwo lim  coll) x y))))
+    (true? (and (<= n lim) (n-mod-x-eq n 12 11) )))
+  (factor [this x y]
+    (let [ n3 (calculate this x y) ]
+      (if (tst this n3)
+        (flipwheel n3 coll)
+        coll))))
 
 
 (defrecord WheelTwo
@@ -31,34 +39,27 @@
   (tst
     [this n]
     (true? (and (<= n lim) (n-mod-x-eq n 12 7))))
-  (factor [this n x y]
+  (factor [this  x y]
     (let [ n2 (calculate this x y) ]
-      (if (tst this n2))
-        (factor (->WheelThree lim  (flipwheel n2 coll) x y))
-        (factor (->WheelThree lim  coll) x y))))
+      (if (tst this n2)
+        (factor (->WheelThree lim  (flipwheel n2 coll)) x y)
+        (factor (->WheelThree lim  coll) x y))) ))
 
 
-
-(defrecord WheelThree
+(defrecord WheelOne
   [lim coll]
   WheelFact
-  (calculate [this x y]  (cond (> x y) (minus (multiply 3 (square x)) (square y) ) :else (inc lim)))
+  (calculate [this x y]  (plus (multiply 4 (square x)) (square y) ))
   (tst
     [this n]
-    (true? (and (<= n lim) (n-mod-x-eq n 12 11) )))
-  (factor [this n x y]
-    (let [ n3 (calculate this x y) ]
-      (if (tst this n3))
-        (flipwheel n3 coll)
-        coll)))
+    (true? (and (<= n lim) (or (n-mod-x-eq n 12 1)  (n-mod-x-eq n 12 5)))))
+  (factor [this  x y]
+    (let [ n1 (calculate this x y) ]
+      (if (tst this n1)
+        (factor (WheelTwo. lim  (flipwheel n1 coll))  x y)
+        (factor (->WheelTwo lim  coll)  x y)))))
 
 
-(defn- flipwheel
-  "toggle boolean value"
-  [n coll]
-  (if (get coll (dec n))
-    (assoc coll (int (dec n)) false)
-    (assoc coll (int (dec n)) true)))
 
 
 
