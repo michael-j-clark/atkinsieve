e829135332b76c1742b8fd66a1587dad9ac6641f
michael-j-clark <github@abstractproduct.com>
2014-05-02 23:26:59 -0500

    Getting somewhere...
    * Cannot debug a secondary Clojure file (wheels.clj) when main (sieve.clj) is running.  Won't go to breakpoints in Idea.
    
    * Must pass coll to functions.  coll cannot be arg to the record.

--- src/atkinsieve/sieve/collgen.clj
+++ src/atkinsieve/sieve/collgen.clj
@@ -3,4 +3,4 @@
 
  ;Had this is sieve.sieve but there was cyclic dependency error bec. is needed in wheels.wheels
 (defn gen-coll "make the collection, a boolean vector w/ first two elems true" [lim]
-  (apply vector (cons 1 (cons 1(take (- lim 2) (repeat false))))))
+  (apply vector (cons true (cons true (take (- lim 2) (repeat false))))))
