776bbfd5ef68de83e7410a34abaf128126224d65
michael-j-clark <github@abstractproduct.com>
2014-05-03 00:38:34 -0500

    take the print statements out

--- src/atkinsieve/wheels/wheels.clj
+++ src/atkinsieve/wheels/wheels.clj
@@ -3,15 +3,13 @@
   (:require [clojure.core.match :refer [match]])
   (:use [atkinsieve.math.mathfns]))
 
-
 (defprotocol WheelFact
   (calculate [this x y])
   (tst [this n])
-  (factor [this x y coll])
-  )
+  (factor [this x y coll]))
 
 (defn flipwheel
-  ;"toggle boolean value"
+  "toggle boolean value"
   [n coll]
   (if (get coll (dec n))
     (assoc coll (int (dec n)) false)
@@ -26,12 +24,10 @@
     (true? (and (<= n lim) (n-mod-x-eq n 12 11) )))
   (factor [this x y coll]
     (let [ n3 (calculate this x y) ]
-      (prn coll "in WheelThree->factor")
       (if (tst this n3)
         (flipwheel n3 coll)
         coll
       ))))
-
 
 (defrecord WheelTwo
   [lim]
@@ -42,11 +38,9 @@
     (true? (and (<= n lim) (n-mod-x-eq n 12 7))))
   (factor [this x y coll]
     (let [ n2 (calculate this x y) ]
-      (prn coll "in WheelTwo->factor")
       (if (tst this n2)
         (factor (->WheelThree lim) x y (flipwheel n2 coll))
         (factor (->WheelThree lim) x y coll))) ))
-
 
 (defrecord WheelOne
   [lim]
@@ -57,7 +51,6 @@
     (true? (and (<= n lim) (or (n-mod-x-eq n 12 1)  (n-mod-x-eq n 12 5)))))
   (factor [this x y coll]
     (let [ n1 (calculate this x y) ]
-      (prn coll "in WheelOne->factor")
       (if (tst this n1)
         (factor (->WheelTwo lim)  x y (flipwheel n1 coll))
         (factor (->WheelTwo lim)  x y  coll)))))
