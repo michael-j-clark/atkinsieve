eaefc84f93b4c6d858fa64402aed9052fe8511ca
michael-j-clark <github@abstractproduct.com>
2014-05-04 02:20:43 -0500

    Changed name to testmathfns.clj

--- src/atkinsieve/math/mathfns.clj
+++ src/atkinsieve/math/mathfns.clj
@@ -5,4 +5,6 @@
 (defn square     [x]     (* x x))
 (defn multiply   [x y]   (* x y))
 (defn plus       [x y]   (+ x y))
+(defn multiply   [x y]   (* x y))
+(defn minus      [x y]   (- x y))
 (defn n-mod-x-eq [n x y] (true? (== (mod n x ) y)))
