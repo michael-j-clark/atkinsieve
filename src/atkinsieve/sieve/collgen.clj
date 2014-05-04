e6d9a7517fa92c6e02b63d7901bda83c8d6aefd2
michael-j-clark <github@abstractproduct.com>
2014-05-04 00:32:03 -0500

    adding (rand-int-vec ...) function

--- src/atkinsieve/sieve/collgen.clj
+++ src/atkinsieve/sieve/collgen.clj
@@ -3,4 +3,6 @@
 
  ;Had this is sieve.sieve but there was cyclic dependency error bec. is needed in wheels.wheels
 (defn gen-coll "make the collection, a boolean vector w/ first two elems true" [lim]
-  (apply vector (cons true (cons true (take (- lim 2) (repeat false))))))
+  (apply vector (cons true (cons true (cons true (take (- lim 3) (repeat false)))))))
+
+(defn rand-int-vec [n lim] (apply vector (take n (repeatedly #(rand-int lim)))))
