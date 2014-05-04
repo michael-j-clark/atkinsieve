20008a00afb9051fcfe37d9a94f7d7920febec0f
michael-j-clark <github@abstractproduct.com>
2014-05-04 03:01:06 -0500

    Using thrush (-->) in tests

--- test/atkinsieve/test/math/testmathfns.clj
+++ test/atkinsieve/test/math/testmathfns.clj
@@ -5,18 +5,18 @@
 
 (deftest squares-test
   (let [r 123]
-    (is (= (map #(*  % %) (range r)) (map square (range r))))))
+    (is (= (->>  (range r) (map #(*  % %))) (->> (range r) (map square))))))
 
 (deftest multiply-test
   (let [q 32289 r 5]
-    (is (= (map #(multiply % q) (range r)) (map #(* % q) (range r))))))
+    (is (= (->> (range r) (map #(multiply % q))) (->> (range r) (map #(* % q)))))))
 
 (deftest minus-test
   (let [q 32289 r 5]
-    (is (= (map #(minus % q) (range r)) (map #(- % q) (range r))))))
+    (is (= (->> (range r) (map #(minus % q))) (->> (range r) (map #(- % q)))))))
 
 (deftest plus-test
   (let [q 1763129 r 5]
-    (is (= (map #(plus % q) (range r)) (map #(+ % q) (range r))))))
+    (is (= (->> (range r) (map #(plus % q))) (->> (range r) (map #(+ % q) ))))))
 
 (run-all-tests)
