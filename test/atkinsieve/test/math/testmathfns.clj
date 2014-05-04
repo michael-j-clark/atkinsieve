47b07463fd9a595fdc42bdf3ebee46e231eebfb6
michael-j-clark <github@abstractproduct.com>
2014-05-04 02:36:29 -0500

    Test cases for atkinsieve.math.mathfns

---
+++ test/atkinsieve/test/math/testmathfns.clj
@@ -0,0 +1,22 @@
+(ns
+  atkinsieve.test.math.mathfns
+  (:use clojure.test)
+  (:use atkinsieve.math.mathfns))
+
+(deftest squares-test
+  (let [r 123]
+    (is (= (map #(*  % %) (range r)) (map square (range r))))))
+
+(deftest multiply-test
+  (let [q 32289 r 5]
+    (is (= (map #(multiply % q) (range r)) (map #(* % q) (range r))))))
+
+(deftest minus-test
+  (let [q 32289 r 5]
+    (is (= (map #(minus % q) (range r)) (map #(- % q) (range r))))))
+
+(deftest plus-test
+  (let [q 1763129 r 5]
+    (is (= (map #(plus % q) (range r)) (map #(+ % q) (range r))))))
+
+(run-all-tests)
