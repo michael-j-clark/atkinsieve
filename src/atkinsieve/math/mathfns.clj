8e46645db4521239056fddb80a8e7217c82da417
michael-j-clark <github@abstractproduct.com>
2014-05-04 20:15:46 -0500

    experimenting w/ :pre & :post

--- src/atkinsieve/math/mathfns.clj
+++ src/atkinsieve/math/mathfns.clj
@@ -2,8 +2,14 @@
   atkinsieve.math.mathfns)
 
 
-(defn square     [x]     (* x x))
-(defn multiply   [x y]   (* x y))
+(defn square     [x]
+  {:pre  [(number? x)]
+   :post [(= % (* x x))]}
+  (* x x))
+(defn multiply   [x y]
+  {:pre  [(number? x)]
+   :post [(= % (+ x y))]}
+  (* x y))
 (defn plus       [x y]   (+ x y))
 (defn multiply   [x y]   (* x y))
 (defn minus      [x y]   (- x y))
