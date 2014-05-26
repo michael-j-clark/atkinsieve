15bc066e52dd7851238ee6f0c3702d419ad857e8
michael-j-clark <github@abstractproduct.com>
2014-05-25 23:31:40 -0500

    Removing bad comments

--- project.clj
+++ project.clj
@@ -3,7 +3,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.match "0.2.1"]
                  [org.clojure/core.typed "0.2.44"]]
-  ;:plugins [[lein-typed "0.3.4"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  ;:test-paths ["test" "test/atkinsieve/test/math"]
   :main atkinsieve.core)
