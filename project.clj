d655e912a5f3dfd8f7438f0268eb917615acfd32
michael-j-clark <github@abstractproduct.com>
2014-05-25 23:30:53 -0500

    Adding dep for core.typed

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject atkinsieve "1.0.0-SNAPSHOT"
   :description "Sieve of Atkin"
-  :dependencies [[org.clojure/clojure "1.3.0"] [org.clojure/core.match "0.2.1"]]
-  :main atkinsieve.sieve.sieve)
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.match "0.2.1"]
+                 [org.clojure/core.typed "0.2.44"]]
+  ;:plugins [[lein-typed "0.3.4"]]
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  ;:test-paths ["test" "test/atkinsieve/test/math"]
+  :main atkinsieve.core)
